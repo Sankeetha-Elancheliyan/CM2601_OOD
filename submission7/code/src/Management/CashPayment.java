32f52438c22e193ceeef69a3c27a4fbb894262c0
Sankeetha-Elancheliyan <119834420+Sankeetha-Elancheliyan@users.noreply.github.com>
2022-12-28 17:42:32 +0530

    Update CashPayment.java
    Not a superclass anymore.

--- submission7/code/src/Management/CashPayment.java
+++ submission7/code/src/Management/CashPayment.java
@@ -1,7 +1,14 @@
 package Management;
 
-public class CashPayment extends Payment{
+public class CashPayment {
     private double balance;
+    static private int paymentId;
+    private String paymentType;
+    private double paymentValue;
+    private Customer customer;
+    private Operator operator;
+    private static int nextpayment_id=0;
+    private ArrayList<Operator> listOfOperator = new ArrayList<>();
 
 
     public CashPayment(int payId, double amount, double balance) {
@@ -9,9 +16,16 @@ public class CashPayment extends Payment{
         this.balance=balance;
     }
     
-    public CashPayment(){
+    public cashPayment(String paymentType, double paymentValue, Customer customer, Operator operator) {
+        this.paymentType = paymentType;
+        this.paymentValue = paymentValue;
+        this.customer = customer;
+        this.operator = operator;
+        this.paymentId = Payment.paymentId;
+        Payment.nextpayment_id +=nextpayment_id;
 
     }
+    
 
     public double getBalance() {
         return balance;
@@ -20,5 +34,51 @@ public class CashPayment extends Payment{
     public void setBalance(double balance) {
         this.balance = balance;
         
+    }
+    
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public double getPaymentValue() {
+        return paymentValue;
+    }
+
+    public void setPaymentValue(double paymentValue) {
+        this.paymentValue = paymentValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                  paymentType +
+                 "-" +paymentValue +
+                 " - " + customer +
+                 " - " + operator +
+                '}';
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
     }
 }
